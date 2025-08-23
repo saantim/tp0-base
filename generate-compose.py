908a6261b4f5c1d56906568c16be95928ffe112c
Santiago Bellido <78931038+saantim@users.noreply.github.com>
2025-08-23 18:57:34 -0300

    Modify script

--- generate-compose.py
+++ generate-compose.py
@@ -15,6 +15,8 @@ SERVER = """
       - LOGGING_LEVEL=DEBUG
     networks:
       - testing_net
+    volumes:
+      - ./server/config.ini:/config.ini
 
 """
 
@@ -39,6 +41,8 @@ CLIENT = """
       - testing_net
     depends_on:
       - server
+    volumes:
+      - ./client/config.yaml:/config.yaml
 """
 
 def parse_args(args):
