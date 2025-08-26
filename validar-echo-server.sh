5c5b13dc9b267d1b395375b04cd2d9b82bf27409
Santiago Bellido <78931038+saantim@users.noreply.github.com>
2025-08-25 23:23:03 -0300

    minnor fix

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -3,10 +3,10 @@
 success="action: test_echo_server | result: success"
 failure="action: test_echo_server | result: fail"
 msg="testing_server"
-output=$(docker run --network=tp0_testing_net busybox /bin/sh -c "echo $msg | nc server 12345")
+response=$(docker run --network=tp0_testing_net busybox /bin/sh -c "echo $msg | nc server 12345")
 
-if [ "$output" = "$msg" ]; then
-  echo success
+if [ "$response" = "$msg" ]; then
+  echo $success
 else
-  echo failure
+  echo $failure
 fi
